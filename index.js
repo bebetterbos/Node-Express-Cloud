f79cc6cb153f081d982b05ad4e89a61460e027bc
bebetterbos <bos34153@gmail.com>
2021-02-10 11:20:33 +0700

    update

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello Node.js World");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
